b33a76d8a6be1411b3af48bcf335fe84458349f5
Galang Eka Wiyono <galangekawiyono@Galangs-MacBook-Air.local>
2026-07-28 13:30:03 +0700

    Initial setup Next.js 15 and Tailwind CSS

---
+++ data/personal.ts
@@ -0,0 +1,15 @@
+export const personalInfo = {
+  name: "Galang Eka Wiyono",
+  role: "Software Quality Assurance Engineer",
+  location: "South Jakarta, Indonesia",
+  email: "ggalangekawiyonoo@gmail.com",
+
+  summary:
+    "Software Quality Assurance Engineer with 5+ years experience in banking and e-commerce systems.",
+
+  linkedin:
+    "https://linkedin.com/in/galangekw",
+
+  github:
+    "https://github.com/sugiorgazdag",
+};
